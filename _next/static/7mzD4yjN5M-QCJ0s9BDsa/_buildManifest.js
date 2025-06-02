8bb6e53badbdb46c2dab0e3a7de6ef2637b9e7ba
aboorde <aboorde@users.noreply.github.com>
2025-06-02 19:58:56 +0000

    deploy: ef94bece69b4306a5dcbe5ec8706017ff4e53159

---
+++ _next/static/7mzD4yjN5M-QCJ0s9BDsa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/296-f5a25a1559e52e97.js","static/chunks/pages/index-3d6d48c3a845840f.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
